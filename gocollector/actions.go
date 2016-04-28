35511d8384b19c4025c6338f34e547587806953b
Walter Doekes <walter+github@wjd.nu>
2016-04-28 15:34:47 +0200

    gocollect: Write core.id.regid with 0400 perms instead of 0644.
    We expect to be run as root, and there is no need to disclose the file
    contents to others on the system. We don't want them to be able to screw
    with our data. And on top of that, we're supposed to write to it once,
    and then leave it.

--- gocollector/actions.go
+++ gocollector/actions.go
@@ -117,7 +117,7 @@ func register(registerUrl string, coreIdData Collected,
 
     os.MkdirAll(path.Dir(regidFilename), 0755)
     err = ioutil.WriteFile(
-        "/var/lib/gocollect/core.id.regid", []byte(value), 0644)
+        "/var/lib/gocollect/core.id.regid", []byte(value), 0400)
     if err != nil {
         logger.Fatal("Could not write core.id.regid: ", err)
         return false
